6d5dcd3801a13fc146c182956be9ffd5ac77b788
Jose Duque <duquepiedrahitaalejandro@gmail.com>
2024-08-07 22:28:45 -0500

    Boton colegio fontan redirige bien, implementando url dinamico...

--- horarioFontanProyecto/estudiantes/urls.py
+++ horarioFontanProyecto/estudiantes/urls.py
@@ -4,6 +4,7 @@ from . import views
 # estudiantes/..
 
 urlpatterns = [
+    path('perfil/<int:num_alumno>', views.num_alumno_vista, name='vista_num_alumno'),
     path('perfil/', views.vista_perfil, name='pagina-estudiantes'),
-    path('', views.vista_estudiantes, name='estudiantes')
+    path('', views.vista_home, name='vista_estudiantes'),
 ]
--- horarioFontanProyecto/estudiantes/views.py
+++ horarioFontanProyecto/estudiantes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 def vista_perfil(request):
@@ -7,6 +7,18 @@ def vista_perfil(request):
     return render(request, 'estudiantes/perfil.html')
 
 
-def vista_estudiantes(request):
+def vista_home(request):
     # estudiantes/templates/estudiantes/home.hmtl
     return render(request, 'estudiantes/home.html')
+
+
+alumnos = {
+    "repre1": 'Nico',
+    "repre2": "Pedro",
+    "repre3": "Jose"
+}
+
+def num_alumno_vista(request, num_alumno):
+    alumno_lista = list(alumnos.values()) # ['Nico', 'Pedro', ...]
+    alumno = alumno_lista[num_alumno]
+    return HttpResponseRedirect(alumno)
