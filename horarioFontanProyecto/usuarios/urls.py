9e2d9064b2e250a9b369552cf5a4b818b091215b
Jose Duque <duquepiedrahitaalejandro@gmail.com>
2024-08-15 10:53:53 -0500

    Boton de icono de perfil ahora es funcional

--- horarioFontanProyecto/usuarios/urls.py
+++ horarioFontanProyecto/usuarios/urls.py
@@ -8,4 +8,5 @@ app_name= 'usuarios'
 urlpatterns = [
     path('', views.inicio, name='vista_inicio'),
     path('registro/', views.registro_usuario, name='vista_registro_usuario'),
+    path('perfil_personal/', views.perfil_modificar, name='perfil_modificar'),
 ]
--- horarioFontanProyecto/usuarios/views.py
+++ horarioFontanProyecto/usuarios/views.py
@@ -33,3 +33,24 @@ def registro_usuario(request):
         form = SignUpForm()
 
     return render(request, 'usuarios/registro.html', {'form': form})
+
+
+def perfil_modificar(request):
+    if request.method == 'POST':
+        user = request.user
+        user.first_name = request.POST.get('nombre')
+        user.last_name = request.POST.get('apellido')
+        user.email = request.POST.get('email')
+        user.username = request.POST.get('username')
+
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+        if password and password == password2:
+            user.set_password(password)
+        
+        user.save()
+        messages.success(request, '¡Perfil actualizado exitosamente!')
+        return redirect('perfil_modificar')
+
+    return render(request, 'usuarios/perfil_personal.html')
