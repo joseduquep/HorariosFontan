aaf1da97ecbfa4502526dda46e335977c76dea4f
Jose Duque <duquepiedrahitaalejandro@gmail.com>
2024-08-16 15:11:50 -0500

    base de datos MYSQL creada y enlazada con DJANGO

--- horarioFontanProyecto/estudiantes/models.py
+++ horarioFontanProyecto/estudiantes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# este es un modelo de prueba para testear la conexion con MySQL
+class TestModel(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"El nombre asignado a este objeto es {self.name}"
--- horarioFontanProyecto/horarioFontanProyecto/settings.py
+++ horarioFontanProyecto/horarioFontanProyecto/settings.py
@@ -75,10 +75,23 @@ WSGI_APPLICATION = 'horarioFontanProyecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'horariosFontan',
+        'USER': 'root',
+        'PASSWORD': '',  # Deja la contraseña vacía
+        'HOST': 'localhost',
+        'PORT': '3306',  # El puerto por defecto de MySQL/MariaDB
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
